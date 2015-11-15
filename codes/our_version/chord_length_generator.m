ef0f7ddd0a2251f0d40efce038588ed7c71f84b9
GlaxyGong <xgongy@gmail.com>
2015-11-15 14:13:06 -0500

    chord length modification

--- codes/our_version/chord_length_generator.m
+++ codes/our_version/chord_length_generator.m
@@ -2,8 +2,8 @@ close all
 clear
 clc
 tic
-filepath='C:\Users\xgong42\Documents\first_72_Test';
-filename=[filepath,'\','1.mat'];
+filepath='C:\Users\xgong42\Documents\first_72_Test';%grain boundary data folder
+filename=[filepath,'\','1.mat'];% plus GBdata .mat file name
 load(filename);
 [hist]=chordlengthdist2(BWdata);
 toc
