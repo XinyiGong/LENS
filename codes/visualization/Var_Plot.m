279c9aa4523fa846a11bae44f15610585b556a64
GlaxyGong <xgongy@gmail.com>
2015-11-20 09:24:20 -0500

    plots

--- codes/visualization/Var_Plot.m
+++ codes/visualization/Var_Plot.m
@@ -1,7 +1,22 @@
+close all
 t=5;
-x=zeros(1,t);
-x(1,1)=Var(1,1);
+x=[1:t];
+y=zeros(1,t);
+y(1,1)=Var(1,1);
 for i=2:t
-    x(1,i)=x(1,i-1)+Var(i,1);
+    y(1,i)=y(1,i-1)+Var(i,1);
 end
-plot(x(1,1:t));
+% plot(x(1,1:t));
+plot(x,100*y); 
+ax=gca;
+set(ax,'XTick',x)
+hold on
+
+grid on
+title('PC vs. Accumulated Variance');
+xlabel('PC','FontSize',12)
+ylabel('% Accumulated Variance','FontSize',12)
+hold on
+% % % % for i=1:t
+text(x,100*y,num2str((100*y)'),'HorizontalAlignment','center');
+% end
--- codes/visualization/pca_visualization_modified.m
+++ codes/visualization/pca_visualization_modified.m
@@ -45,56 +45,107 @@ prm=prm(I,:);
 cd('/Users/Gong/Documents/LENS/results/Plot_72-structure_2pt_PCA')
 
 % Loading the PCA matrix
-load 72pt_PC.mat
+load 72_2pt_PC.mat
 
 % Specifying the parameter (this is the parameter you will select for
 % different visualization
-parameter=4;
+parameter=1;
+parameter2=3;
+parameter3=2;
+parameter4=4;
 % Finding the unique numbers in that parameters (number of different
 % classes and eventually the number of colors in visualizations
 pl=unique(prm(:,parameter));
+pl2=unique(prm(:,parameter2));
+pl3=unique(prm(:,parameter3));
+pl4=5;
 % Generating a colormap for PCA visualization. If you have only 2 different
 % numbers in one parameter, the visualization will have only 2 colors
-cmap=hsv(length(pl));
+cmap=hsv(length(pl)*length(pl2)*length(pl3));
+fg=1;
 
-figure
-parleg={};
-subplot(1,2,1)
-for ii=1:length(pl)
-    plot3(PC(prm(:,parameter)==pl(ii),1),PC(prm(:,parameter)==pl(ii),2),...
-        PC(prm(:,parameter)==pl(ii),3),'kd','MarkerFaceColor',cmap(ii,:),'MarkerSize',8)
-    hold on
-    parleg=[parleg; [char(parleglist(parameter)) ' ' num2str(pl(ii))]];
-end
-grid on
-axis equal
-xlabel('PC 1','FontSize',12)
-ylabel('PC 2','FontSize',12)
-zlabel('PC 3','FontSize',12)
-set(gca,'FontSize',12)
-h=legend(parleg,'Orientation','Horizontal');
-set(h,'FontSize',12);
-
+for pl3l=1:length(pl3)
+    if pl3l<2
+for jj=1:length(pl2)
+    figure(fg);
+    fg=fg+1;
+    parleg={};
+    kkk=1;
+    for kk=1:jj
+        for ii=1:length(pl)
+            showprm=logical((prm(:,parameter)==pl(ii)).*(prm(:,parameter2)==pl2(kk)).*(prm(:,parameter3)==pl3(kkk)).*(prm(:,parameter4)==pl4));
+            plot3(PC(showprm,1),PC(showprm,2),...
+                PC(showprm,3),'kd','MarkerFaceColor',cmap(ii+length(pl)*(kk-1)+length(pl)*length(pl2)*(kkk-1),:),'MarkerSize',8)
+            hold on
+        parleg=[parleg; [char(parleglist(parameter3)) ' ' num2str(pl3(kkk)) ' ' char(parleglist(parameter2)) ' ' num2str(pl2(kk)) ' ' char(parleglist(parameter)) ' ' num2str(pl(ii))]];
+        end
+    end
+        
+    grid on
+    axis equal
+    xlabel('PC 1','FontSize',12)
+    ylabel('PC 2','FontSize',12)
+    zlabel('PC 3','FontSize',12)
+    set(gca,'FontSize',12)
+    
+    h=legend(parleg,'Orientation','Vertical');
+    set(h,'FontSize',12);
 % Axis image just scales the axes of the image. It makes it more realistic
 % but if you dont want it you can basically turn it off by sending those
 % lines to comment.
 
 % parleg variable is just recursively written inside the for loop for the
 % automatic creation of legend.
-
-subplot(1,2,2)
-for ii=1:length(pl)
-    plot3(PC(prm(:,parameter)==pl(ii),1),PC(prm(:,parameter)==pl(ii),2),...
-        PC(prm(:,parameter)==pl(ii),3),'kd','MarkerFaceColor',cmap(ii,:),'MarkerSize',8)
-    hold on
-    
 end
-grid on
-axis equal
-xlabel('PC 1','FontSize',12)
-ylabel('PC 2','FontSize',12)
-zlabel('PC 3','FontSize',12)
-set(gca,'FontSize',12)
+    else
+    jj=length(pl2)
+    figure(fg);
+    fg=fg+1;
+    parleg={};
+    for kkk=1:pl3l
+    for kk=1:jj
+        for ii=1:length(pl)
+            showprm=logical((prm(:,parameter)==pl(ii)).*(prm(:,parameter2)==pl2(kk)).*(prm(:,parameter3)==pl3(kkk)).*(prm(:,parameter4)==pl4));
+            plot3(PC(showprm,1),PC(showprm,2),...
+                PC(showprm,3),'kd','MarkerFaceColor',cmap(ii+length(pl)*(kk-1)+length(pl)*length(pl2)*(kkk-1),:),'MarkerSize',8)
+            hold on
+        parleg=[parleg; [char(parleglist(parameter3)) ' ' num2str(pl3(kkk)) ' ' char(parleglist(parameter2)) ' ' num2str(pl2(kk)) ' ' char(parleglist(parameter)) ' ' num2str(pl(ii))]];
+        end
+    end
+    end
+    grid on
+    axis equal
+    xlabel('PC 1','FontSize',12)
+    ylabel('PC 2','FontSize',12)
+    zlabel('PC 3','FontSize',12)
+    set(gca,'FontSize',12)
+    
+    h=legend(parleg,'Orientation','Vertical');
+    set(h,'FontSize',12);
+% Axis image just scales the axes of the image. It makes it more realistic
+% but if you dont want it you can basically turn it off by sending those
+% lines to comment.
+
+% parleg variable is just recursively written inside the for loop for the
+% automatic creation of legend.
+    end
+
+end
+% % % % subplot(1,2,2)
+% % % % for ii=1:length(pl)
+% % % %     plot3(PC(prm(:,parameter)==pl(ii),1),PC(prm(:,parameter)==pl(ii),2),...
+% % % %         PC(prm(:,parameter)==pl(ii),3),'kd','MarkerFaceColor',cmap(ii,:),'MarkerSize',8)
+% % % %     hold on
+% % % %     
+% % % % end
+% % % % grid on
+% % % % axis equal
+% % % % xlabel('PC 1','FontSize',12)
+% % % % ylabel('PC 2','FontSize',12)
+% % % % zlabel('PC 3','FontSize',12)
+% % % % set(gca,'FontSize',12)
+
+
 
 % save process parameter information correspond to PC matrix
 prm=prm(:,1:4);
