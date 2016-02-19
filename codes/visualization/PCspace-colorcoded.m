e6d3913eb26b565f1f5184cfc00a48c1fd45a16a
GlaxyGong <xgongy@gmail.com>
2016-02-19 13:51:25 -0500

    2pt-PCA
    adding Var_PC plots and color coded PC plots

--- codes/visualization/PCspace-colorcoded.m
+++ codes/visualization/PCspace-colorcoded.m
@@ -1,11 +1,11 @@
 % PP7list(:,1)=(PP7(:)==5);
 % PP7list(:,2)=(PP7(:)==20);
 % PP7list(:,3)=(PP7(:)==35);
-for i=1:72
+% % for i=1:72
 fig=figure;
-filename=['plot',num2str(i)];
+% % filename=['plot',num2str(i)];
 scatter3(PC(showprmo1,1),PC(showprmo1,2),PC(showprmo1,3),20*ones(length(showprmo1),1),'r','filled');
-view(5*i,30)
+% % view(5*i,30)
 grid on
 xlabel('PC 1','FontSize',20)
 ylabel('PC 2','FontSize',20)
@@ -17,5 +17,5 @@ hold on
 scatter3(PC(showprmon,1),PC(showprmon,2),PC(showprmon,3),20*ones(length(showprmon),1),'b','filled');
 hold on
 scatter3(PC(showprmo2,1),PC(showprmo2,2),PC(showprmo2,3),20*ones(length(showprmo2),1),'g','filled');
-print(fig,filename,'-dpng');
-end
+% % print(fig,filename,'-dpng');
+% % end
--- codes/visualization/Var_Plot.m
+++ codes/visualization/Var_Plot.m
@@ -13,12 +13,12 @@ set(ax,'XTick',x)
 hold on
 
 grid on
-title('PC vs. Accumulated Variance','FontSize',36);
-xlabel('PC','FontSize',36)
-ylabel('% Accumulated Variance','FontSize',36)
-set(gca,'FontSize',36)
+title('PC vs. Accumulated Variance','FontSize',18);
+xlabel('PC','FontSize',18)
+ylabel('% Accumulated Variance','FontSize',18)
+set(gca,'FontSize',18)
 axis([1 10 70 100])
 hold on
 % % % % for i=1:t
-text(x(1:3),100*y(1:3),num2str((100*y(1:3))'),'HorizontalAlignment','center','FontSize',36);
+text(x(1:3),100*y(1:3),num2str((100*y(1:3))'),'HorizontalAlignment','center','FontSize',18);
 % end
