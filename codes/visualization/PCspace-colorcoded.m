2326eadb4e2a66d71592fd46ccec0c422fc65141
GlaxyGong <xgongy@gmail.com>
2016-02-19 16:14:48 -0500

    uploading color coded PC plots

--- codes/visualization/PCspace-colorcoded.m
+++ codes/visualization/PCspace-colorcoded.m
@@ -1,11 +1,13 @@
-% PP7list(:,1)=(PP7(:)==5);
-% PP7list(:,2)=(PP7(:)==20);
-% PP7list(:,3)=(PP7(:)==35);
+PPlist(:,1)=(prm(:,7)==5);
+PPlist(:,2)=(prm(:,7)==20);
+PPlist(:,3)=(prm(:,7)==35);
+
 % % for i=1:72
 fig=figure;
 % % filename=['plot',num2str(i)];
-scatter3(PC(showprmo1,1),PC(showprmo1,2),PC(showprmo1,3),20*ones(length(showprmo1),1),'r','filled');
-% % view(5*i,30)
+s1=scatter3(PC(PPlist(:,1),1),PC(PPlist(:,1),2),PC(PPlist(:,1),3),20*ones(length(PC(PPlist(:,1),3)),1),'r','filled');
+% 20*ones(length(PPlist(:,1)),1),
+view(-135,65)
 grid on
 xlabel('PC 1','FontSize',20)
 ylabel('PC 2','FontSize',20)
@@ -14,8 +16,11 @@ set(gca,'FontSize',20)
 % h=title('Procee Parameter - Principle Component');
 % set(h,'FontSize',12);
 hold on
-scatter3(PC(showprmon,1),PC(showprmon,2),PC(showprmon,3),20*ones(length(showprmon),1),'b','filled');
+s2=scatter3(PC(PPlist(:,2),1),PC(PPlist(:,2),2),PC(PPlist(:,2),3),20*ones(length(PC(PPlist(:,2),3)),1),'b','filled');
+% 20*ones(length(PPlist(:,2)),1),
 hold on
-scatter3(PC(showprmo2,1),PC(showprmo2,2),PC(showprmo2,3),20*ones(length(showprmo2),1),'g','filled');
+s3=scatter3(PC(PPlist(:,3),1),PC(PPlist(:,3),2),PC(PPlist(:,3),3),20*ones(length(PC(PPlist(:,3),3)),1),'g','filled');
+% 20*ones(length(PPlist(:,3)),1),
 % % print(fig,filename,'-dpng');
 % % end
+legend([s1 s2 s3],'1','2','3')
