1acf2cec65bbc9334fabe6905bc1c286f7bc7a95
GlaxyGong <xgongy@gmail.com>
2016-02-28 17:02:00 -0500

    models for 2pt-PCA
    also get ready for hard split

--- codes/visualization/chord_length_visualization.m
+++ codes/visualization/chord_length_visualization.m
@@ -6,7 +6,7 @@ clc
 % % % cd('/Users/Gong/Desktop/chord_length_72');
 
 i=1;
-plotend=100;
+plotend=50;
 
 % % % load(['chordlength_',num2str(i),'.mat']);
 
@@ -20,17 +20,19 @@ for j=1:3
 %     else
 %         color='b';
 %     end
-    h(j)=plot(1:plotend,hist(1:plotend,j),'Color', colorstring(j));
+    h(j)=plot(1:plotend,str189(1:plotend,j),'Color', colorstring(j));
     hold on;
 end
 
 grid on
 xlabel('Chord Length','FontSize',36)
-ylabel('Weighted Frequency','FontSize',36)
+ylabel('Frequency','FontSize',36)
 % set(legend(h(1:6),'X Direction','Y Direction','Z Direction','X Edge','Y Edge','Z Edge'),'FontSize',12);
 set(legend(h(1:3),'X Direction','Y Direction','Z Direction'),'FontSize',36);
 % axis([0 300 0 0.09])
-title('Weighted Chord Length Distribution','FontSize',36)
+title('V=2.5 W=90','FontSize',36)
+set(gca,'fontsize',36)
+axis([0 50 0 0.25])
 
 % % % subplot(1,2,2)
 % % % plot(1:plotend,hist(1:plotend,4))
