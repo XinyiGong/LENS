420166c98c608f51862a5fcddefd3715fd5fdb9c
GlaxyGong <xgongy@gmail.com>
2016-03-04 01:23:47 -0500

    Getting error for comparison

--- Code/visualization/StatComparison.m
+++ Code/visualization/StatComparison.m
@@ -1,6 +1,6 @@
 % Define paths and names
-saveerrorpath = '/Users/Gong/Desktop/LENS/Data/Visual/HardSplit_W70/';
-saveerrorname = 'error_CLD2_NE_W70';
+saveerrorpath = '/Users/Gong/Desktop/LENS/Data/Visual/HardSplit_V7p5/';
+saveerrorname = 'error_CLD2_NE_V7p5';
 
 % Compare GG(CLD statistics)
 cmp = GG-GGbp;
@@ -8,7 +8,7 @@ GGnmerror=zeros(size(GG,1),1);
 GGerror=zeros(size(GG,1),1);
 for i = 1:size(GG,1)
     GGnmerror(i,1) = norm(cmp(i,:),2)/norm(GG(i,:),2);
-    GGerror(1,i) = sqrt(norm(cmp(i,:),2)*norm(cmp(i,:),2)/size(cmp,2));
+    GGerror(i,1) = sqrt(norm(cmp(i,:),2)*norm(cmp(i,:),2)/size(cmp,2));
 end
 MAnmE = mean(GGnmerror(:,1));
 MAE = mean(GGerror(:,1));
