4c2f39bf7aed9692535164c291766b50a474d8fb
GlaxyGong <xgongy@gmail.com>
2016-03-17 10:38:40 -0400

    exclude d75

--- Code/Modeling/modelselect_PP134567_PC123_deg3.m
+++ Code/Modeling/modelselect_PP134567_PC123_deg3.m
@@ -1,11 +1,11 @@
-load('C:\Users\xgong42\Documents\LENS\Data\PC\PC_ChLOrig_HC_V7p5W70');
-load('C:\Users\xgong42\Documents\LENS\Data\PP\metadata_ChLOrig_HC_V7p5W70');
-savepath = 'C:\Users\xgong42\Documents\LENS\Data\PPtoPCModels\';
+load('C:\Users\xgong42\Google Drive\Data\PC\PC_ChLOrig_HC_xd75_V7p5W70');
+load('C:\Users\xgong42\Google Drive\Data\PP\metadata_ChLOrig_HC_xd75_V7p5W70');
+savepath = 'C:\Users\xgong42\Google Drive\Data\PPtoPCModels\';
 
 prm = prms{1,2};
 tic
-for pccn = 1:1
-    modelname = ['model_PP134567_PC',num2str(pccn),'_deg3_ChLOrig_HC_V7p5W70'];
+for pccn = 1:10
+    modelname = ['model_PP134567_PC',num2str(pccn),'_deg3_ChLOrig_HC_xd75_V7p5W70'];
     reg = ModelSelect(prm(:,[1 3 4 5 6 7]),PC(:,pccn),3);
     save([savepath,modelname],'reg');
 end
